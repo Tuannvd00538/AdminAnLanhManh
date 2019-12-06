fa6495ca33fb0748bed103a84f439e4a82793cf6
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-12-07 02:04:09 +0700

    edit order list

--- src/app/components/order/list-order/list-order.component.ts
+++ src/app/components/order/list-order/list-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-list-order',
@@ -9,7 +11,20 @@ export class ListOrderComponent implements OnInit {
 
   constructor() { }
 
+  token: string = localStorage.getItem('token');
+
   ngOnInit() {
+    const that = this;
+    if (this.token == null || this.token == undefined) {
+      window.location.href = '/auth/login';
+      return;
+    }
+    axios.get(`${environment.api_url}/api/order`, { headers: { Authorization: that.token } }).then((response) => {
+      console.log(response.data);
+
+    }).catch((err) => {
+      console.log(err);
+    })
   }
 
 }
